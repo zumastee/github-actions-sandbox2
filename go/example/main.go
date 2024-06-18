ed68d3c37145ea1b20f3ca4f67709e7783468f3a
zumax <32590194+zumastee@users.noreply.github.com>
2024-06-18 23:36:16 +0900

    Bug fixed.

--- go/example/main.go
+++ go/example/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 
-var version string # ビルド時にidflas フラグ経由でバージョンを埋め込む
+var version string // ビルド時にidflas フラグ経由でバージョンを埋め込む
 
 func main() {
 	fmt.Println("Example: %s¥n", version)
